f85a5f329c9fbff334685446ef9a131b46abbc2d
BykanovM <byandyby@gmail.com>
2024-04-14 20:00:15 +0300

    string strcmp

--- libs/data_structures/string/string_.h
+++ libs/data_structures/string/string_.h
@@ -27,4 +27,9 @@ char *findNonSpaceReverse(char *rbegin, const char *rend);
 //возвращается адрес rend
 char *findSpaceReverse(char *rbegin, const char *rend);
 
+//возвращает отрицательное значение, если lhs располагается до rhs
+//в лексикографическом порядке, значение 0, если lhs и rhs
+//равны, иначе – положительное значение
+int strcmp(const char *lhs, const char *rhs);
+
 #endif //ARRAY_STRING__H
