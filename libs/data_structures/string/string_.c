d75e566585a431f6cb83220566596564d47ab580
BykanovM <byandyby@gmail.com>
2024-04-14 19:50:21 +0300

    string findSpaceReverse

--- libs/data_structures/string/string_.c
+++ libs/data_structures/string/string_.c
@@ -42,3 +42,11 @@ char *findNonSpaceReverse(char *rbegin, const char *rend) {
 
     return rbegin;
 }
+
+char *findSpaceReverse(char *rbegin, const char *rend) {
+    while (rbegin != rend && !isspace(*rbegin)) {
+        rbegin--;
+    }
+
+    return rbegin;
+}
