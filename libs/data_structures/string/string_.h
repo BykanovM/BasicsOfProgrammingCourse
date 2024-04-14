beabbb59957e3c64611ec5b4c2066aec8a33e667
BykanovM <byandyby@gmail.com>
2024-04-14 18:40:10 +0300

    string find

--- libs/data_structures/string/string_.h
+++ libs/data_structures/string/string_.h
@@ -1,7 +1,11 @@
 #ifndef ARRAY_STRING__H
 #define ARRAY_STRING__H
 
-//возвращает количество символов в строке не считая ноль-символ.
+//возвращает количество символов в строке не считая ноль-символ
 size_t strlen_(const char *begin);
+
+//возвращает указатель на первый элемент с кодом ch, расположенным
+//на ленте памяти между адресами begin и end не включая end
+char* find(char *begin, char *end, int ch);
 
 #endif //ARRAY_STRING__H
