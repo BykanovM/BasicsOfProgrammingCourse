c74e29391cfa3bc89d614c2478f7062d494a2a0e
BykanovM <byandyby@gmail.com>
2024-05-12 20:26:24 +0300

    task 9

--- libs/algorithms/algorithms.c
+++ libs/algorithms/algorithms.c
@@ -102,3 +102,103 @@ void evaluateExpression(const char *filename) {
 
     fclose(file);
 }
+
+#define MAX_LENGTH_STRING 200
+
+#define MAX_AMOUNT_SPORTSMAN 20
+
+static void generate_name(char* s) {
+    int name_length = rand() % 30 + 5;
+
+    char* rec_ptr = s;
+    for (int i = 0; i < name_length; i++) {
+        *rec_ptr = rand() % 26 + 97;
+        rec_ptr++;
+    }
+    *rec_ptr = '\0';
+}
+
+void generateTeam(const char* filename, const int n) {
+    srand(time(NULL));
+
+    FILE* file = fopen(filename, "wb");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < n; i++) {
+        sportsman s;
+        generate_name(s.name);
+
+        s.best_result = (double) rand() / 100;
+
+        fwrite(&s, sizeof(sportsman),1, file);
+    }
+
+    fclose(file);
+}
+
+void sortSportsman(sportsman sm[], const int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (sm[j].best_result < sm[j + 1].best_result) {
+                sportsman temp = sm[j];
+                sm[j] = sm[j + 1];
+                sm[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void getBestTeam(const char* filename, const int n) {
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    sportsman* team = (sportsman*) malloc(MAX_AMOUNT_SPORTSMAN * sizeof(sportsman));
+
+    sportsman* rec_ptr = team;
+    int amount_sportsman = 0;
+    while (fread(rec_ptr, sizeof(sportsman), 1, file) == 1) {
+        rec_ptr ++;
+        amount_sportsman++;
+    }
+
+    fclose(file);
+
+    file = fopen(filename, "wb");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    sortSportsman(team, amount_sportsman);
+
+    int amount_in_team = amount_sportsman >= n ? n : amount_sportsman;
+
+    for (int i = 0; i < amount_in_team; i++) {
+        fwrite(team + i, sizeof(sportsman), 1, file);
+    }
+
+    free(team);
+    fclose(file);
+}
+
+void printTeam(const char* filename) {
+    FILE* file = fopen(filename, "rb");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    sportsman s;
+    while (fread(&s, sizeof(sportsman), 1, file) == 1) {
+        printf("%s %lf", s.name, s.best_result);
+        printf("\n");
+    }
+
+    fclose(file);
+}
--- libs/algorithms/algorithms.h
+++ libs/algorithms/algorithms.h
@@ -5,4 +5,23 @@ void generateExpression(const char *file_name);
 
 void evaluateExpression(const char *filename);
 
+#define MAX_LENGTH_STRING 200
+
+#define MAX_AMOUNT_SPORTSMAN 20
+
+typedef struct sportsman {
+    char name[MAX_LENGTH_STRING];
+    double best_result;
+} sportsman;
+
+static void generate_name(char* s);
+
+void generateTeam(const char* filename, const int n);
+
+void sortSportsman(sportsman sm[], const int n);
+
+void getBestTeam(const char* filename, const int n);
+
+void printTeam(const char* filename);
+
 #endif //UNTITLED1_ALGORITHMS_H
