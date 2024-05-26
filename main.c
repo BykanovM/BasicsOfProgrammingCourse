cf13b87a0caa918aef0994ec1cb42b5f8c5e6734
BykanovM <byandyby@gmail.com>
2024-05-26 14:56:36 +0300

    lab 20 task 2 test

--- main.c
+++ main.c
@@ -3510,9 +3510,11 @@ void testUpdateProductOrderLessProduct() {
 }*/
 
 #include "libs/algorithms/tasks/fill_matrix.h"
+#include "libs/algorithms/tasks/game_life.h"
 
 void test() {
     test_fillMatrix();
+    test_gameLife();
 }
 
 int main() {
