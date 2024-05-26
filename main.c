c3ba4ebdd1306538e3e20caa581676f7720f963a
BykanovM <byandyby@gmail.com>
2024-05-26 16:40:06 +0300

    lab 20 task 4 test

--- main.c
+++ main.c
@@ -3512,11 +3512,13 @@ void testUpdateProductOrderLessProduct() {
 #include "libs/algorithms/tasks/fill_matrix.h"
 #include "libs/algorithms/tasks/game_life.h"
 #include "libs/algorithms/tasks/median_filter.h"
+#include "libs/algorithms/tasks/get_domains.h"
 
 void test() {
     test_fillMatrix();
     test_gameLife();
     test_medianFilter();
+    test_getDomains();
 }
 
 int main() {
